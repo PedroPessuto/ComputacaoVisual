5dc323ca6cad4c5b3f835d4a4a0d40bd3366efde
Pedro Pessuto <pedro.pessuto@outlook.com>
2025-09-15 19:17:55 -0300

    Add main.c with initial team comments and main function

---
+++ main.c
@@ -0,0 +1,11 @@
+// ========= EQUIPE ========= 
+// Henrique Árabe Neres de Farias - 10410152
+// Ian Miranda da Cunha  - 10409669
+// Pedro Pessuto Rodrigues Ferreira - 10409729
+// ========================== 
+
+int main() {
+  
+  return 0;
+  
+}
